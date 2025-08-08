0a515861224b77b88143ec29940604ff3f65f7a3
Chris-de-sypher <christiansilvester700@gmail.com>
2025-08-08 02:48:50 -0700

    Update server.js
    Updated again

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,11 @@ require("ejs");
 require("./config/db");
 
 // http server
-// const { createServer } = require("http");
-// const server = createServer(app);
+const { createServer } = require("http");
+const server = createServer(app);
 
 // socket.io import (FIX)
-// const { Server } = require("socket.io");
+const { Server } = require("socket.io");
 
 // routes
 const authRoutes = require("./routes/auth.routes");
@@ -85,23 +85,23 @@ app.use(
 );
 
 // // ✅ FIXED SOCKET.IO INITIALIZATION
-// const io = new Server(server, {
-//   cors: {
-//     origin: "*",
-//   },
-// });
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 // socket events
-// io.on("connection", (socket) => {
-//   console.log("connection established and stable");
+io.on("connection", (socket) => {
+  console.log("connection established and stable");
 
-//   socket.on("disconnect", () => {
-//     console.log("connection lost and user disconnected");
-//   });
-// });
+  socket.on("disconnect", () => {
+    console.log("connection lost and user disconnected");
+  });
+});
 
 // start server
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
 
